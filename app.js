1e6539cfa0cf1caebc69a3038aa9af90f50ff342
Nishanta Khadka <nisxant69@gmail.com>
2025-06-17 16:08:21 +0545

    Day 11 Progress || Middleware Implementation

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const { isLoggedin } = require("./middleware/isLoggedin")
 const cookieParser = require("cookie-parser")   
 app.use(cookieParser())
 
-app.get("/",async (req,res)=>{
+
+app.get("/", isLoggedin,async (req,res)=>{
     const datas = await db.todoModel.findAll()
     res.render("todo/get_todo",{datas:datas})
 })
 app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true}))
 
-
-
 app.get("/add", isLoggedin, function(req,res){
     res.render("todo/add_todo")
 })
-app.get("/update",function(req,res){
+app.get("/update",isLoggedin,function(req,res){
     res.render("todo/update_todo")
 })
 app.get("/login",function(req,res){
@@ -29,11 +28,11 @@ app.get("/register",function(req,res){
     res.render("authentication/register")
 })
 
+
 //Form bata value input leko
 app.post('/register', async (req, res) => {
     const { username, email, password } = req.body
     console.log(req.body)
-
     await db.userModel.create({
         username: username,
         email: email,
@@ -49,31 +48,30 @@ app.post("/login",async (req,res)=>{
             email : email
         }
     })
-    
     if(users.length == 0){
         res.send("Email not registered")
     } else {
         //Check Password, first  plain password , second hashed password table ma store vako
         const isPasswordMatch =  bcrypt.compareSync(password,users[0].password)
         if(isPasswordMatch){
-            const token=jwt.sign({name:"nishant"},"secretkeyhola",{   //payload secondma private key
+            const token=jwt.sign({id:users[0].id},"secretkey",{   //payload secondma private key
                 expiresIn:"1d" //Kati time ma expire hunxa
             })
             res.cookie("token",token)
             // res.send(token)
             // res.send("Logged in Successfully")
-            
         } else{
             res.send("Invalid Email or Password")
         }
     }
     res.redirect("/")
 })
+
+
 //Todo Store garne
 app.post('/addtodo', async (req, res) => {
     const { task, description, date } = req.body
     console.log(req.body)
-
     await db.todoModel.create({
         task: task,
         description : description,
@@ -81,7 +79,6 @@ app.post('/addtodo', async (req, res) => {
     })
     res.send("Todo List Updated  Successfully")
 })
-
 
 
 app.listen(3000,function(){
--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,20 +1,21 @@
 const jwt = require("jsonwebtoken")
 
-exports.isLoggedin = (req,res)=>{
+exports.isLoggedin = (req,res,next)=>{
      console.log("isLoggedIn function is triggered")
      //RECEIVE Token
      const token = req.cookies.token
-
      //If No Token
      if(!token){
         res.send("Please Login First")
      }else{
         //Verify the token
-        jwt.verify(token,"secretkeyhola",(error,result)=>{
+        jwt.verify(token,"secretkey",(error,result)=>{
             if(error){
                 res.send("Invalid Token")
             } else{
-                res.send("TOKEN VALID VERIFIED ! !")
+                //req.age=25 yesari data pathauna sakinxa 
+                console.log(result)
+                next()
             }
         })
      }
